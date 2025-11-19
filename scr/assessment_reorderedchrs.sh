96d7235f0618e5a309c25088c2098a3bffbaf48f
Nicolò Tellini <nicolo.tellini@unice.fr>
2025-11-19 16:16:02 +0100

    Update assessment_reorderedchrs.sh

--- scr/assessment_reorderedchrs.sh
+++ scr/assessment_reorderedchrs.sh
@@ -12,7 +12,7 @@ ref_pathfile=$(cat $inds".sort.mash" | cut -f2 | head -1)
 gff_pathfile=$(echo $ref_pathfile | sed 's+.fa+.gff3+g')
 
 #conda activate mummer
-nucmer -t 10 -p $inds".end" $ref_pathfile $inds".genome.fa"
+nucmer -p $inds".end" $ref_pathfile $inds".genome.fa"
 delta-filter -1 $inds".end.delta" >  $inds".end.delta_filter"
 mummerplot --large --color --png $inds".end.delta_filter" -p $inds".end"
 #conda deactivate
