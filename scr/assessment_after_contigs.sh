306df35572b6277c1f1ea5d6c2d22ffbf1ace68d
Nicolò Tellini <nicolo.tellini@unice.fr>
2025-11-19 16:15:15 +0100

    Update assessment_after_contigs.sh

--- scr/assessment_after_contigs.sh
+++ scr/assessment_after_contigs.sh
@@ -12,7 +12,7 @@ ref_pathfile=$(cat $inds".sort.mash" | cut -f2 | head -1)
 gff_pathfile=$(echo $ref_pathfile | sed 's+.fa+.gff3+g')
 
 #conda activate mummer
-nucmer -t 10 -p $inds".flye.clean" $ref_pathfile $inds".flye.clean.fa"
+nucmer -p $inds".flye.clean" $ref_pathfile $inds".flye.clean.fa"
 delta-filter -1 $inds".flye.clean.delta" >  $inds".flye.clean.delta_filter"
 mummerplot --large --color --png $inds".flye.clean.delta_filter" -p $inds".flye.clean"
 #conda deactivate
